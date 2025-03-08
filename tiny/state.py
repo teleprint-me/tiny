ba0d3d04fd78719ab4434171fdfffda8b5a39a57
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-03-08 16:59:40 -0500

    state(feat): Add initial outline for model state implementation

--- tiny/state.py
+++ tiny/state.py
@@ -8,23 +8,30 @@ Only relevant configuration parameters should be stored alongside the weights.
 Common parameters between training and inference are considered relevant parameters.
 """
 
+from dataclasses import dataclass
+
 import torch
 import torch.nn as nn
 from torch.optim.adamw import AdamW
 from torch.optim.optimizer import Optimizer
 
+from tiny.config import TinyConfig
 from tiny.model import TinyTransformer
 from tiny.tokenizer import TinyTokenizer
 
 
 class TinyState:
-    tokenizer: TinyTokenizer
-    transformer: TinyTransformer
+    def __init__(self, model_path: str, model_config: dataclass = None):
+        self.path = model_path
+        self.config = model_config
+        self.tokenizer = TinyTokenizer()
+        self.checkpoint = None
+        self.model = None
 
     def create_optimizer(self) -> Optimizer:
         """Return the AdamW optimizer."""
         return AdamW(
-            self.transformer.parameters(recurse=self.config.recurse),
+            self.model.parameters(recurse=self.config.recurse),
             lr=self.config.lr,
             eps=self.config.eps,
             weight_decay=self.config.weight_decay,
@@ -44,3 +51,25 @@ class TinyState:
             return torch.load(self.model_path)
         except (FileNotFoundError,):
             return {}
+
+    def load_model(self) -> TinyTransformer:
+        self.checkpoint = self.create_checkpoint()
+
+        # Do **not** override user configuration
+        if not self.config and "config" in self.checkpoint:
+            self.config = TinyConfig(**self.checkpoint["config"])
+
+        # Create the model
+        self.model = TinyTransformer(self.config)
+
+        # Load the state dict if it exists
+        if "state" in self.checkpoint:
+            self.model.load_state_dict(self.checkpoint["state"])
+
+        # Move the model to the available device
+        self.model.to(self.config.device)
+
+        return self.model
+
+    def save_model(self) -> None:
+        pass
