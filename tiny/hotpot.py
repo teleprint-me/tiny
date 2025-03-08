fbb71688a9e8c9326357afe05c3dd8aeaa814633
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-03-08 01:27:55 -0500

    hotpot(chore): Apply fixes to improve the readability of the docstring

--- tiny/hotpot.py
+++ tiny/hotpot.py
@@ -1,13 +1,11 @@
 """
-Script: hotpot-to-tiny.py
+Script: tiny.hotpot
 Description: Downloads and converts the Hotpot QA to a simple a tiny format.
 
----
-
-We only need a few hundred samples at most. Any more than that is overkill.
-Ideally, we just grab 100 samples random. The output file is formatted for
-input-target pairs where the question becomes the input and the answer becomes
-the target.
+This script downloads and converts Hotpot QA data into a simplified format suitable for
+training a small-scale model. The goal is to obtain around 100 random samples, where each
+sample is represented as an input-target pair with the question serving as the input and
+the answer as the target.
 """
 
 import argparse
