3a1b9f4f3f9c8806c5001d660a863852acdf2c47
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-03-08 00:15:42 -0500

    model(feat): Add positional encodings model

--- tiny/model.py
+++ tiny/model.py
@@ -14,6 +14,40 @@ import torch
 from torch import nn
 
 
+class PositionalEncoding:
+    """Standard fixed sinusoidal positional encoding for transformer models."""
+
+    def __init__(self, d_model: int, max_seq: int, dtype: torch.dtype):
+        super().__init__()
+        self.d_model = d_model
+        self.max_seq = max_seq
+        self.dtype = dtype
+
+        # Register precomputed positional encodings as a non-trainable buffer
+        self.register_buffer("pe", self._generate_sinusoidal_encoding())
+
+    # ref: https://stackoverflow.com/a/77445896/20035933
+    def _generate_sinusoidal_encoding(self) -> torch.Tensor:
+        """Creates sinusoidal positional encodings as described in Vaswani et al. (2017)."""
+        # Create a tensor of shape (max_seq, d_model) to store the positional encodings.
+        pe = torch.zeros(self.max_seq, self.d_model)
+        # Create a tensor of shape (max_seq, 1) to store the position indices.
+        position = torch.arange(0, self.max_seq, dtype=self.dtype).unsqueeze(1)
+        # Calculate the division term of shape (D/2,) to avoid division by zero.
+        d = torch.arange(0, self.d_model, 2, dtype=self.dtype)
+        t = -torch.log(torch.tensor(10000.0)) / self.d_model
+        div_term = torch.exp(d * t)
+        # Compute the positional encoding for even and odd indices separately.
+        pe[:, 0::2] = torch.sin(position * div_term)  # sin(position * (10000 ** (2i / d_model))
+        pe[:, 1::2] = torch.cos(position * div_term)  # cos(position * (10000 ** (2i / d_model))
+        return pe.unsqueeze(0)  # (1, T, D)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Add positional encoding to input tensor x."""
+        seq_len = x.size(1)
+        return x + self.pe[:, :seq_len, :]  # (B, T, D)
+
+
 class LayerNormalization(nn.Module):
     def __init__(self, d_model: int, eps: float = 1e-6):
         super().__init__()
