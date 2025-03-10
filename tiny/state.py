3ddf4f437a3b0e6661841163a8f3c99ad975e8a5
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-03-10 02:09:07 -0400

    state(feat): Add logging to TinyState

--- tiny/state.py
+++ tiny/state.py
@@ -37,11 +37,15 @@ class TinyState:
     def load_checkpoint(self) -> None:
         """Return the model state dict."""
         try:
+            self.logger.info(f"Loading checkpoint from {self.config.model_path}.")
             self.checkpoint = torch.load(self.config.model_path)
         except (FileNotFoundError,):
+            self.logger.info(f"No checkpoint found. Creating new state at {self.config.model_path}")
             self.checkpoint = {}
 
     def load_model(self) -> None:
+        self.logger.debug("Loading model state...")
+
         # Load dependencies
         self.load_tokenizer()
         self.load_dataset()
@@ -50,19 +54,28 @@ class TinyState:
         # Override select parameters between runs
         if "config" in self.checkpoint:
             self.config.set_frozen_params(self.checkpoint["config"])
+            self.logger.info("Model config loaded from checkpoint.")
 
         # Create the model
         self.model = TinyTransformer(self.config)
+        self.logger.info("Model created: TinyTransformer")
 
         # Load the state dict if it exists
         if "state" in self.checkpoint:
             self.model.load_state_dict(self.checkpoint["state"])
+            self.logger.info("Model state loaded from checkpoint.")
 
         # Move the model to the available device
         self.model.to(self.config.device)
+        self.logger.info(f"Model moved to device: {self.config.dname}")
 
     def save_model(self) -> None:
         """Save the model state along with relevant configuration."""
+        self.logger.debug("Saving model state...")
+
+        if self.model is None:
+            raise RuntimeError("Model is not initialized, cannot save checkpoint.")
+
         state_dict = {
             "config": self.config.get_frozen_params(),
             "state": self.model.state_dict(),
